3e1a05a51e524249055d85bc874c7933be11ca87
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-06 14:57:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,8 @@
-def loop_message_five_times (message)
+def loop_message_five_times(message)
   count = 0 
   while count <5 do
-    puts messagecount += 1
+    puts message
+    count += 1
   end
 end
 
